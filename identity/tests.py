7c299be56cb4e5137d2445ad96d6adc9499d5e05
niekas <albertasgim@gmail.com>
2020-06-26 20:16:38 +0300

    Add test to check if all form fields were provided

--- identity/tests.py
+++ identity/tests.py
@@ -1,3 +1,5 @@
+import io
+
 from django.test import TestCase
 from django.urls import reverse
 
@@ -11,11 +13,7 @@ class IdentityFormTestCase(TestCase):
         self.assertEqual(resp.status_code, 200)
 
     def test_empty_form_submition(self):
-        resp = self.client.post(
-            self.index_url,
-            data={},
-        )
-
+        resp = self.client.post(self.index_url, data={})
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.content.count(b'class="errorlist"'), 4)
         self.assertIn(b'This field is required.', resp.content)
@@ -27,11 +25,23 @@ class IdentityFormTestCase(TestCase):
             "birth_date": "2020-01-01",
         }
 
-        resp = self.client.post(
-            self.index_url,
-            data=data,
-        )
+        resp = self.client.post(self.index_url, data=data)
 
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.content.count(b'class="errorlist"'), 1)
         self.assertIn(b'This field is required.', resp.content)
+
+    def test_successful_form_submition(self):
+        data = {
+            "name": "Vardenis",
+            "surname": "Pavardenis",
+            "birth_date": "2020-01-01",
+            "document_picture": io.BytesIO(b'test_file_content'),
+        }
+
+        resp = self.client.post(self.index_url, data=data)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn(b'You have entered correct information!', resp.content)
+        self.assertNotIn(b'class="errorlist"', resp.content)
+        self.assertNotIn(b'This field is required.', resp.content)
