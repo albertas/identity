79ae3b71493fb97f23b520ab0f57b5d31675fa9c
niekas <albertasgim@gmail.com>
2020-06-26 20:08:11 +0300

    Add tests for partial form input submition

--- identity/tests.py
+++ identity/tests.py
@@ -3,7 +3,35 @@ from django.urls import reverse
 
 
 class IdentityFormTestCase(TestCase):
+    def setUp(self):
+        self.index_url = reverse('index')
+
     def test_index_page(self):
-        index_url = reverse('index')
-        resp = self.client.get(index_url)
+        resp = self.client.get(self.index_url)
         self.assertEqual(resp.status_code, 200)
+
+    def test_empty_form_submition(self):
+        resp = self.client.post(
+            self.index_url,
+            data={},
+        )
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.content.count(b'class="errorlist"'), 4)
+        self.assertIn(b'This field is required.', resp.content)
+
+    def test_text_field_form_submition(self):
+        data = {
+            "name": "Vardenis",
+            "surname": "Pavardenis",
+            "birth_date": "2020-01-01",
+        }
+
+        resp = self.client.post(
+            self.index_url,
+            data=data,
+        )
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.content.count(b'class="errorlist"'), 1)
+        self.assertIn(b'This field is required.', resp.content)
