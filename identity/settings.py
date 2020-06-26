7ffc1ab538d6e0931f18c352523be6ee3fbb507c
niekas <albertasgim@gmail.com>
2020-06-26 20:22:34 +0300

    Add REALID_API_KEY variable to django settings
    its value is loaded from OS environment variables in order not to expose secrets.

--- identity/settings.py
+++ identity/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# RealID API key
+REALID_API_KEY = os.environ.get('REALID_API_KEY', 'test_key')
